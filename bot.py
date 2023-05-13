478700ee556bf2d74dd25606e8edd1e5065ee17b
Aviral Srivastava <79373546+Gregnald@users.noreply.github.com>
2023-05-13 19:24:47 +0530

    .

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from os import environ
+from Bard import Chatbot
+
+token = environ.get("BARD_TOKEN")
+with open("bis.txt", "r") as f:
+    TOKEN = f.readline().strip()
+
+with open("tris.txt", "r") as f:
+    token = f.readline().strip()
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("Bot is online!")
+
+@client.event
+async def on_message(message):
+    if message.content == "ping":
+        await message.channel.send(f"Pong! {message.author.display_name}")
+
+# Run the bot
+client.run(TOKEN)
